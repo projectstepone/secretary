b99ab22578f82cfca542d941a8a8b15baefd5c2e
Shashank Sinha <shashsinboy@gmail.com>
2021-04-27 14:28:42 +0530

    Add housekeeping endpoint

--- src/main/java/io/appform/secretary/resources/BulkDataResource.java
+++ src/main/java/io/appform/secretary/resources/BulkDataResource.java
@@ -2,7 +2,6 @@ package io.appform.secretary.resources;
 
 import io.appform.secretary.command.FileDataDBCommand;
 import io.appform.secretary.executor.FileDataExecutor;
-import io.appform.secretary.model.FileData;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -14,15 +13,12 @@ import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
-import java.util.List;
 
 @Slf4j
 @Path("/v1/data")
@@ -44,23 +40,13 @@ public class BulkDataResource {
                                     @Valid @NotNull @FormDataParam("file") FormDataContentDisposition fileMetaData,
                                     @Valid @NotBlank @FormDataParam("user") String userId,
                                     @Valid @NotBlank @FormDataParam("flow") String workflow) {
-        log.info("Received upload request: Filename : {} Workflow: {} UserId: {}",
+        log.info("Request: Upload filename : {} workflowId: {} userId: {}",
                 fileMetaData.getName(), workflow, userId);
 
         //TODO: Convert input to object using validator; perfom sanity check on workflow and file hashsum
         executor.processFile(fileStream, fileMetaData.getFileName(), workflow, userId);
 
+        log.info("Response: Successfully processed file: {}", fileMetaData.getName());
         return Response.ok().build();
-    }
-
-    @GET
-    @Path("file/{userId}")
-    public Response getFilesByUser(@Valid @NotBlank @PathParam("userId") String userId) {
-        log.info("Received request to list files uploaded by user: {}", userId);
-
-        List<FileData> data = dbCommand.getByUser(userId);
-
-        log.info("List of files uploaded by user {}: {}", userId, data);
-        return Response.ok().entity(data).build();
     }
 }
